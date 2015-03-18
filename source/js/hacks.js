6c214307dd4e8fea9a9a4575af34913a3f508238
Rob  Easthope <robeasthope@gmail.com>
2015-03-18 17:13:24 +0000

    dd D3 demo js code

--- source/js/hacks.js
+++ source/js/hacks.js
@@ -1,84 +1,152 @@
 "use strict";
 
+// PubNub Credentials
 var pubnubCongig = require('./pubnub-credentials.json');
 
-// PubNub voting app demo code
-$(function() {
-	window.data = [{
-		name: "Amanda",
-		vote: 0
-	}, {
-		name: "Katie",
-		vote: 0
-	}, {
-		name: "Janice",
-		vote: 0
-	}, {
-		name: "Kim",
-		vote: 0
-	}, {
-		name: "Amy",
-		vote: 0
-	}];
-	window.pubnub = PUBNUB.init({
-		channel: "survey",
-		publish_key: "pubnubConfig.publish_key",
+(function() {
+
+	/* Events from UI */
+
+
+	var region = document.getElementById('region');
+	var picker = document.getElementById('dataCenterPicker');
+	var dataCenter = picker.options[picker.selectedIndex].value;
+
+	picker.addEventListener('change', function(e){
+		dataCenter = picker.options[picker.selectedIndex].value;
+		console.log(dataCenter);
+	}, false);
+
+	var isRunning = true;
+	var button = document.getElementById('toggle');
+
+	button.addEventListener('click', function(e){
+		if(isRunning) {
+			pubnub.unsubscribe({
+				channel: channel
+			});
+			button.value = 'Stream again';
+			isRunning = false;
+		} else {
+			getData();
+			button.value = 'Stop me!';
+			isRunning = true;
+		}
+		
+	}, false);
+
+
+
+	/* D3 Bubble Chart */
+
+	var diameter = Math.min(document.getElementById('chart').clientWidth, window.innerHeight - document.querySelector('header').clientHeight) - 20;
+
+	var svg = d3.select('#chart').append('svg')
+		.attr('width', diameter)
+		.attr('height', diameter);
+
+	var bubble = d3.layout.pack()
+		.size([diameter, diameter])
+		.value(function(d) {return d.size;}) // new data is loaded to bubble layout
+		.padding(3);
+
+	function drawBubbles(m) {
+
+		if(m.region !== dataCenter) return;
+
+		region.textContent = m.region;
+
+		// generate data with calculated layout values
+		var nodes = bubble.nodes(processData(m))
+			.filter(function(d) { return !d.children; }); // filter out the outer bubble
+
+		// assign new data to existing DOM 
+		var vis = svg.selectAll('circle')
+			.data(nodes, function(d) { return d.name; });
+
+		// enter data -> remove, so non-exist selections for upcoming data won't stay -> enter new data -> ...
+
+		// To chain transitions, 
+		// create the transition on the updating elements before the entering elements 
+		// because enter.append merges entering elements into the update selection
+
+		var duration = 500;
+		var delay = 0;
+
+		// update - this is created before enter.append. it only applies to updating nodes.
+		vis.transition()
+			.duration(duration)
+			.delay(function(d, i) {delay = i * 7; return delay;}) 
+			.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; })
+			.attr('r', function(d) { return d.r; })
+			.style('opacity', 1); // force to 1, so they don't get stuck below 1 at enter()
+
+		// enter - only applies to incoming elements (once emptying data)	
+		vis.enter().append('circle')
+			.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; })
+			.attr('r', function(d) { return 0; })
+			.attr('class', function(d) { return d.className; })
+			.transition()
+			.duration(duration * 1.2)
+			.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; })
+			.attr('r', function(d) { return d.r; })
+			.style('opacity', 1);
+
+		// exit
+		vis.exit()
+			.transition()
+			.duration(duration)
+			.attr('transform', function(d) { 
+				var dy = d.y - diameter/2;
+				var dx = d.x - diameter/2;
+				var theta = Math.atan2(dy,dx);
+				var destX = diameter * (1 + Math.cos(theta) )/ 2;
+				var destY = diameter * (1 + Math.sin(theta) )/ 2; 
+				return 'translate(' + destX + ',' + destY + ')'; })
+			.attr('r', function(d) { return 0; })
+			.remove();
+	}
+
+
+
+	
+	/* PubNub */
+
+	var channel = 'rts-xNjiKP4Bg4jgElhhn9v9';
+
+	var pubnub = PUBNUB.init({
 		subscribe_key: "pubnubConfig.subscribe_key"
 	});
-	pubnub.subscribe({
-		channel: 'survey',
-		message: increment
-	});
 
-	function sendData(msg) {
-		pubnub.publish({
-			channel: 'survey',
-			message: msg
+	function getData() {
+		var i = 0;
+		pubnub.subscribe({
+			channel: channel,
+			callback: function(m) {
+				// i++; 
+				// if(i === 1 || i%10 === 0) {
+				// 	drawBubbles(m);
+				// }	
+				drawBubbles(m);		
+			}
 		});
 	}
 
-	function draw(data) {
-		var bars = d3.select(".container").selectAll(".bar-wrapper").data(data);
-		var barEnter = bars.enter().append("div").attr("class", "bar-wrapper") barEnter.append("button").text(function(d) {
-			return "Vote " + d.name;
-		}).attr("class", "vote-btn btn-default btn-primary").on("click", function(d) {
-			sendData(d.name);
-		});
-		barEnter.append("div").attr("class", "bar").style("width", function(d) {
-			return (d.vote * 10) + 15 + "px";
-		}).text(function(d) {
-			return d.vote
-		});
-		bars.selectAll("div").text(function(d) {
-			return d.vote
-		}).style("width", function(d) {
-			return (d.vote * 10) + 15 + "px";
-		});
-		bars.exit().remove()
-	};
+	function processData(data) {
+		if(!data) return;
 
-	function increment(msg) {
-		for (var i = 0; i < window.data.length; i++) {
-			var el = window.data[i];
-			if (el.name == msg) {
-				el.vote += 1;
-			}
+		var obj = data.countries_msg_vol;
+
+		var newDataSet = [];
+
+		for(var prop in obj) {
+			newDataSet.push({name: prop, className: prop.toLowerCase().replace(/ /g,''), size: obj[prop]});
 		}
-		draw(data);
+		return {children: newDataSet};
 	}
 
-	function init_votes() {
-		pubnub.history({
-			channel: 'survey',
-			start: 0,
-			callback: function(msg) {
-				var vote_history = msg[0];
-				for (var i = 0; i < vote_history.length; i++) {
-					increment(vote_history[i]);
-				}
-			}
-		});
-	}
-	init_votes();
-	draw(data);
-});
+
+
+	getData();
+	
+})();
