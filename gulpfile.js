a8a9f6252b67272ff2f6766edacd16d700e6d625
Rob  Easthope <robeasthope@gmail.com>
2015-03-18 16:36:30 +0000

    Fix typo

--- gulpfile.js
+++ gulpfile.js
@@ -135,7 +135,7 @@ gulp.task('dev', gulp.parallel('build', 'watch', 'browser-sync'), function () {
 
 // PUBLISH TASKS
 // Publish the app to S3
-gulp.task('publish-app', gulp.series('build') function() {
+gulp.task('publish-app', gulp.series('build'), function() {
 	// create a new publisher 
 	var publisher = awspublish.create({
 		"bucket": "aws.bucket",
